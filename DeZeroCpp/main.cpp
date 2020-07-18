6c70ab8d3dd40adce4d6676103e6ef5b1e3adeaa
bugph0bia <64964079+bugph0bia@users.noreply.github.com>
2020-07-19 00:03:58 +0900

    ステップ22

--- DeZeroCpp/main.cpp
+++ DeZeroCpp/main.cpp
@@ -28,11 +28,12 @@ namespace step18 { extern void step18(); }
 namespace step19 { extern void step19(); }
 namespace step20 { extern void step20(); }
 namespace step21 { extern void step21(); }
+namespace step22 { extern void step22(); }
 
 int main()
 {
 	// 標準出力の小数点以下桁数を 15 とする
 	std::cout << std::fixed << std::setprecision(15);
 
-	step21::step21();
+	step22::step22();
 }
